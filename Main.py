d9473b1e0f0be65223e705fa4eb3dba4bf9f77cc
t8j <git@t8j.monster>
2020-11-13 14:58:28 -0700

    optimized code to use more functions

--- Main.py
+++ Main.py
@@ -8,45 +8,32 @@ bot = commands.Bot(command_prefix='>')
 
 
 # FUNCTIONS
-async def add_bal(amount, id):
+async def getval(id, file, ifno):
     try:
         os.mkdir(id)
     except OSError:
         print('')
     try:
-        bal = open(id + '/bal.txt', 'r')
+        bal = open(id + '/' + str(file) + '.txt', 'r')
     except OSError:
-        bal = open(id + '/bal.txt', 'a+')
-        bal.write('0')
+        bal = open(id + '/' + str(file) + '.txt', 'a+')
+        bal.write(ifno)
         bal.close()
-        bal = open(id + '/bal.txt', 'r')
-    tempbal = bal.read()
-    bal.close()
-    bal = open(id + '/bal.txt', 'w')
-    bal.write(str(int(tempbal) + int(amount)))
-
-
-async def bald(id):
-    try:
-        os.mkdir(id)
-    except OSError:
-        print('')
-    try:
-        bal = open(id + '/bal.txt', 'r')
-    except OSError:
-        bal = open(id + '/bal.txt', 'a+')
-        bal.write('0')
-        bal.close()
-        bal = open(id + '/bal.txt', 'r')
+        bal = open(id + '/' + str(file) + '.txt', 'r')
     balnum = bal.read()
-    print(bal.read())
     return balnum
+
+
+async def addval(id, file, add):
+    temp = await getval(str(id), file, '0')
+    file2 = open(id + '/' + str(file) + '.txt', 'w')
+    file2.write(str(int(temp) + int(add)))
 
 
 # COMMANDS
 @bot.command()
 async def bal(ctx):  # command to check balance
-    balnum = await bald(str(ctx.author.id))
+    balnum = await getval(str(ctx.author.id), 'bal', '0')
     if balnum == '0':
         await ctx.send('yikes he\'s poor')
     await ctx.send('you have ' + balnum + '$')
@@ -56,20 +43,12 @@ async def bal(ctx):  # command to check balance
 async def daily(ctx):  # command to get a daily allowance
     today = date.today()
     today2 = str(today.strftime("%Y%m%d"))
-    try:
-        os.mkdir(str(ctx.author.id))
-    except OSError:
-        print('')
-    day = open(str(ctx.author.id) + '/daily.txt', 'a+')
-    day.close()
-    day = open(str(ctx.author.id) + '/daily.txt', 'r')
-    maybetoday = day.read()
+    maybetoday = await getval(str(ctx.author.id), 'daily', '0')
     if maybetoday == today2:
         await ctx.send('it hasn\'t been a day dumbass')
     else:
-        await add_bal('2500', str(ctx.author.id))
+        await addval(str(ctx.author.id), bal, '2500')
         await ctx.send('I gotchu fam')
-    day.close()
     day = open(str(ctx.author.id) + '/daily.txt', 'w')
     day.write(today2)
 
@@ -77,7 +56,7 @@ async def daily(ctx):  # command to get a daily allowance
 @bot.command()
 async def grant(ctx, id, amount):  # admin command to grant money
     if ctx.author.id == 295349622765912086:
-        await add_bal(str(amount), str(id))
+        await addval(str(id), 'bal', str(amount))
         await ctx.send('granted ' + str(amount) + '$ to id ' + str(id))
 
 
