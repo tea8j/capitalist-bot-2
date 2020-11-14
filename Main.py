db71294ab41d445a1bb2103c74d18f146a54dd71
t8j <git@t8j.monster>
2020-11-13 23:18:32 -0700

    added pet inventory and buying, optimized code

--- Main.py
+++ Main.py
@@ -1,7 +1,11 @@
+# created by tea8j on github, if this was reposted elsewhere, tell me.
+# credits to magic turtle#6942 on discord for help testing and brainstorming
+# TODO: add dog, add pet functionality, add inventory system
 import discord
 from discord.ext import commands
 import os
 from datetime import date
+import pickle
 
 # sets command prefix
 bot = commands.Bot(command_prefix='>')
@@ -9,25 +13,57 @@ bot = commands.Bot(command_prefix='>')
 
 # FUNCTIONS
 async def getval(id, file, ifno):
-    try:
-        os.mkdir(id)
-    except OSError:
-        print('')
+    await mkdir(id)
     try:
         bal = open(id + '/' + str(file) + '.txt', 'r')
     except OSError:
         bal = open(id + '/' + str(file) + '.txt', 'a+')
-        bal.write(ifno)
+        bal.write(str(ifno))
         bal.close()
         bal = open(id + '/' + str(file) + '.txt', 'r')
     balnum = bal.read()
     return balnum
 
 
+async def mkdir(dir):
+    try:
+        os.mkdir(str(dir))
+    except OSError:
+        print('')
+
+
 async def addval(id, file, add):
     temp = await getval(str(id), file, '0')
     file2 = open(id + '/' + str(file) + '.txt', 'w')
     file2.write(str(int(temp) + int(add)))
+
+
+async def buypetfunc(id, pet, cost):
+    if int(await getval(id, 'bal', '0'))>int(cost)-1:
+        temp = open(id + '/petlist.txt', 'a+')
+        temp.close()
+        with open(str(id) + '/petlist.txt', 'rb') as fp:
+            try:
+                load = pickle.load(fp)
+            except EOFError:
+                load = None
+            if await getval(str(id), 'maxpets', 3)==load:
+                return 'inventory full ('+str(await getval(id, 'maxpets', 3))+'/'+str(await getval(id, 'maxpets', 3))+')'
+            else:
+                try:
+                    load.append(str(pet))
+                except:
+                    load=[pet]
+                with open(str(id) + '/petlist.txt', 'wb') as file:
+                    pickle.dump(load, file)
+                await addval(str(id), bal, '-' + cost)
+                return str(pet)+' bought for '+str(cost)+'$'
+    else: return 'I diagnose you with poor'
+
+
+def converttostr(input_seq, seperator):
+   final_str = seperator.join(input_seq)
+   return final_str
 
 
 # COMMANDS
@@ -47,7 +83,8 @@ async def daily(ctx):  # command to get a daily allowance
     if maybetoday == today2:
         await ctx.send('it hasn\'t been a day dumbass')
     else:
-        await addval(str(ctx.author.id), bal, '2500')
+        temp = (int(1000 * float(await getval(str(ctx.author.id), 'dailymultiply', '1'))))
+        await addval(str(ctx.author.id), bal, temp)
         await ctx.send('I gotchu fam')
     day = open(str(ctx.author.id) + '/daily.txt', 'w')
     day.write(today2)
@@ -58,6 +95,24 @@ async def grant(ctx, id, amount):  # admin command to grant money
     if ctx.author.id == 295349622765912086:
         await addval(str(id), 'bal', str(amount))
         await ctx.send('granted ' + str(amount) + '$ to id ' + str(id))
+
+
+
+@bot.command()
+async def buypet(ctx, pet = None):
+    if not pet:
+        with open('petshop.txt', 'r') as petshop:
+            await ctx.send(petshop.read())
+    else:
+        if pet == 'turtle':
+            await ctx.send(await buypetfunc(str(ctx.author.id), 'turtle', '5000'))
+            await ctx.send(':turtle:')
+@bot.command()
+async def petlist(ctx):
+    with open(str(ctx.author.id) + '/petlist.txt', 'rb') as fp:
+        list = pickle.load(fp)
+        stringlist = converttostr(list, ', ')
+        await ctx.send(stringlist)
 
 
 # tells you when ready and changes discord activity
